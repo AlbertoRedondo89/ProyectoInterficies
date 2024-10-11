3c364040e7dbf6ea7e8dea237b0357d7b583eadf
Berto <albertoredondo@paucasesnovescifp.cat>
2024-10-11 13:38:19 +0200

    Creado el proyecto
    Creación del proyecto en netbeans

---
+++ ProyectoInterficies/src/main/java/spdvi/proyectointerficies/ProyectoInterficies.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package spdvi.proyectointerficies;
+
+/**
+ *
+ * @author alber
+ */
+public class ProyectoInterficies {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
